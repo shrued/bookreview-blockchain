ae9e8d57b42d3587709936516c4acdc55701cb88
Shruti <52789421+ShrutiSara@users.noreply.github.com>
2021-11-01 08:30:01 +0530

    basic blockchain

---
+++ BookReview/urls.py
@@ -0,0 +1,28 @@
+"""blockchain_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from books import views
+from books.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url('^get_review$', views.get_review, name="get_review"),
+    url('^add_review$', views.add_review, name="add_review"),
+    url('^is_valid$', views.is_valid, name="is_valid"),
+]
